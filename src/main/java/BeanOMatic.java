b10d62184007e86ae606b079a403c5f2d1925f83
markthegrea <mjuchems@gmail.com>
2021-02-16 10:32:18 -0600

    first blush

---
+++ src/main/java/BeanOMatic.java
@@ -0,0 +1,11 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class BeanOMatic {
+
+    public static void main(String[] args) {
+        System.out.println("mark");
+
+        StringUtils.isNotBlank("mark");
+    }
+    
+}
